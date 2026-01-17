455805001ba217cb941d803448b9b38151bdc69b
YC酱luyancib <luyancib@qq.com>
2026-01-17 22:50:03 +0800

    docs: drop some useless plugin

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,24 +4,16 @@ import DefaultTheme from 'vitepress/theme-without-fonts'
 import { useData,useRoute } from 'vitepress'
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import { NolebaseHighlightTargetedHeading } from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
-import {
-  NolebaseEnhancedReadabilitiesMenu,
-  NolebaseEnhancedReadabilitiesScreenMenu
-} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 import { NolebaseInlineLinkPreviewPlugin } from '@nolebase/vitepress-plugin-inline-link-preview/client'
 import codeblocksFold from 'vitepress-plugin-codeblocks-fold'
-import imageViewer from 'vitepress-plugin-image-viewer'
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
 
 / plugin css /
-import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 import '@nolebase/vitepress-plugin-enhanced-mark/client/style.css'
 import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css'
 import 'vitepress-plugin-codeblocks-fold/style/index.css'
 import 'markdown-it-autospace/spacing.css'
-import 'viewerjs/dist/viewer.min.css'
 
 / font & style css /
 import { css } from './MisansVF.ttf'
@@ -35,22 +27,18 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'layout-top': () => [h(NolebaseHighlightTargetedHeading)],
-      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
-      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
+      'layout-top': () => [h(NolebaseHighlightTargetedHeading)]
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
     app.use(css),
     app.use(NolebaseGitChangelogPlugin),
-    app.use(NolebaseInlineLinkPreviewPlugin),
-    app.component('vImageViewer',vImageViewer)
+    app.use(NolebaseInlineLinkPreviewPlugin)
   },
   setup() {
     const { frontmatter } = useData();
     const route = useRoute();
     codeblocksFold({frontmatter,route});
-    imageViewer(route)
   }
 }
