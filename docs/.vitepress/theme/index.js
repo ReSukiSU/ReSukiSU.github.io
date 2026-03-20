824cbf1f4d5a4db001e25f5c5a5bac85ef383f08
YC酱luyancib <luyancib@qq.com>
2026-03-20 20:20:28 +0800

    Refactor error page rendering with cloudflare-error-page (#18)
    * clean line
    
    * add cloudflare-error-page dependencies
    
    * render it!
    
    * simply and remove useless compoment
    
    Signed-off-by: YC酱luyancib <luyancib@qq.com>
    
    * small change
    
    ---------
    
    Signed-off-by: YC酱luyancib <luyancib@qq.com>

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,5 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
+import { defineComponent, h } from "vue";
 import DefaultTheme from "vitepress/theme-without-fonts";
 import { useData, useRoute } from "vitepress";
 import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
@@ -7,6 +7,7 @@ import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-hig
 import { NolebaseInlineLinkPreviewPlugin } from "@nolebase/vitepress-plugin-inline-link-preview/client";
 import codeblocksFold from "vitepress-plugin-codeblocks-fold";
 import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
+import { render as render_cf_error_page } from 'cloudflare-error-page';
 
 /* plugin css */
 import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
@@ -22,10 +23,49 @@ import "remixicon/fonts/remixicon.css";
 import "./style.css";
 import "./fonts.css";
 
+const FullScreen404 = defineComponent({
+  setup() {
+    const html = render_cf_error_page({
+    title: "Page not found",
+    error_code: "404",
+    error_source: "Cloudflare",
+    cloudflare_status: {
+      location: "Vitepress",
+      name: "Somebody",
+      status: "error",
+      status_text: "Set up us the bomb.",
+    },
+    what_happened: "The page you requested does not exist.",
+    what_can_i_do: "Check the URL or <a href=\"/\">return to the homepage</a>.",
+    perf_sec_by: {
+      text: "ReSukiSU Development Team",
+      link: "https://github.com/ReSukiSU",
+    },
+    more_information:{
+      text: "ReSukiSU Development Team Issue Tracker",
+      link: "https://github.com/ReSukiSU/ReSukiSU.github.io/issues",
+    }
+  })
+    return () => h("div", {
+      innerHTML: html,
+      style: {
+        width: "100%",
+        minheight: "100vh",
+        position: "absolute",
+        overflow: "auto",
+      },
+    });
+  },
+});
+
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    if (useData().page.value.isNotFound) {
+      return h(FullScreen404);
+    }
+    
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
       "layout-top": () => [h(NolebaseHighlightTargetedHeading)],
